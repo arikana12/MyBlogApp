03024e2d588534bcf37bd6a971339b8c668696d5
arikana12 <arikana.358code@gmail.com>
2023-08-13 16:32:58 +0900

    プロジェクトを新規作成しました

---
+++ MyBlogApp/Models/Article.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MyBlogApp.Models
+{
+    public class Article
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [DisplayName("タイトル")]
+        public string Title { get; set; }
+
+        [Required]
+        [DisplayName("本文")]
+        public string Body { get; set; }
+
+        public DateTime Created {  get; set; }
+
+
+
+
+
+
+
+    }
+}
